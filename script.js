06058d6f75685b8d97bf3e3f3ce0bb213e908077
Lu69as <lukasokken@gmail.com>
2024-11-28 10:23:25 +0100

    Improved the code for weekends Going to start work on a date countdown mode

--- script.js
+++ script.js
@@ -1,23 +1,3 @@
-let weekend = [5, 15, 30, 0];
-if (localStorage.getItem("hour") != null)
-    weekend[1] = localStorage.getItem("hour");
-
-if (localStorage.getItem("minute") != null)
-    weekend[2] = localStorage.getItem("minute");
-
-weekend[2]--;
-let tempHour = weekend[1];
-
-
-document.querySelector("input.hour").addEventListener("change", () => { 
-    localStorage.setItem("hour", document.querySelector("input.hour").value); 
-    location.reload();
-});
-document.querySelector("input.minute").addEventListener("change", () => { 
-    localStorage.setItem("minute", document.querySelector("input.minute").value); 
-    location.reload();
-});
-
 let myCanvas = document.createElement('canvas');
 document.querySelector('#celebrate').appendChild(myCanvas);
 function celebrate() {
@@ -35,73 +15,112 @@ function celebrate() {
 }; 
 let celebratecount = 0;
 
-function newTime() {
-    let date = new Date();
-
-    // Day
-    {
-        if (weekend[0] - date.getDay() < 0) {
-            document.querySelector(".day .time").innerHTML = date.getDay() - weekend[0];
-            document.querySelector("h1.title").innerHTML = "DET ER HELG!";
-            document.querySelector(".text.before").innerHTML = "Det er bare";
-            document.querySelector(".text.after").innerHTML = "Til helgen er over";
-
-            if (celebratecount < 1) {
-                celebrate();
-                celebratecount++;
-            }
-        }
-        else if (weekend[0] - date.getDay() == 0) {
-            document.querySelector(".day").style.display = "none";
-            document.querySelector("h1.title").innerHTML = "Nå er vi så jævlig nærme!";
-            document.querySelector(".text.before").innerHTML = "Det er bare";
-            document.querySelector(".text.after").innerHTML = "Til helgen nå";
-        }
-        else if (weekend[0] - date.getDay() == 1) {
-            document.querySelector(".day .time").innerHTML = weekend[0] - date.getDay();
-            document.querySelector("h1.title").innerHTML = "Endelig snart helg nå!";
-            document.querySelector(".text.before").innerHTML = "Det er enda";
-        }
-        else {
-            document.querySelector(".day .time").innerHTML = weekend[0] - date.getDay();
-        }
+function nextWeekend(start) {
+    let tempdate = new Date().getDay();
+    let addition = 0;
+    while (tempdate < start) {
+        addition++;
+        tempdate++;
     }
+    while (tempdate > start) {
+        addition--;
+        tempdate--;
+    }
+    
+    return new Date().getDate() + addition;
+}
 
-    // Hour
-    {
-        if (weekend[0] == date.getDay() && ((weekend[1] - date.getHours() == 0 && weekend[2] < date.getMinutes()) || weekend[1] - date.getHours() <0)) {
-            document.querySelectorAll(".time-container").forEach((e) => e.style.display = "none")
-            document.querySelector("h1.title").innerHTML = "DET ER HELG!";
-            document.querySelector(".text.before").innerHTML = "Enda bedre,";
-            document.querySelector(".text.after").innerHTML = "DET ER FREDAG!";
+let countdownType;
+let today = new Date();
+let countdownDate = new Date(today.getFullYear(), today.getMonth(), nextWeekend(5), 15, 30);
 
-            if (celebratecount < 1) {
-                celebrate();
-                celebratecount++;
-            }
-        }
-        else if (weekend[1] - date.getHours() < 0) {
-            document.querySelector(".hour .time").innerHTML = weekend[1] - date.getHours() + 24;
-        }
-        else if ((weekend[1] - date.getHours() == 0 || (weekend[1] - date.getHours() == 1 && weekend[2] < date.getMinutes())) && weekend[0] - date.getDay() == 0) {
+if (countdownDate.getDay() - today.getDay() >= -1 && countdownDate.getDay() - today.getDay() <= 5)
+    countdownType = "weekend";
+
+function updateTimeWeekend(countdownDate) {
+    let now = new Date();
+    let days = countdownDate.getDay() - now.getDay();
+    let hours = countdownDate.getHours() - now.getHours();
+        if (hours > countdownDate.getHours() && days > 0) { hours = 24 + hours; days--;};
+    let minutes = countdownDate.getMinutes() - now.getMinutes();
+        if (minutes > countdownDate.getMinutes() && hours > 0) { hours--; };
+        if (minutes < 0 && hours > 0) { minutes = 60 + minutes; hours--; };
+        if (minutes < 0 && hours == 0 && days > 0) { minutes = 60 + minutes; hours = 23 + hours; days--; };
+    let seconds = 59 - now.getSeconds();
+
+    document.querySelector(".day .time").innerHTML = days;
+    document.querySelector(".hour .time").innerHTML = hours;
+    document.querySelector(".minute .time").innerHTML = minutes;
+    document.querySelector(".second .time").innerHTML = seconds;
+    document.querySelector(".millisecond .time").innerHTML = Math.floor((1000 - new Date().getMilliseconds()) / 10);
+
+    document.querySelectorAll(".time-container").forEach(e => e.style.display = "flex" )
+    document.querySelectorAll(".time-container:not(.millisecond)").forEach(e => e.style.display = "flex" )
+
+    document.querySelector(".time-container.millisecond").style.display = "none";
+
+    if (days == 0) {
+        document.querySelector(".day").style.display = "none";
+
+        document.querySelector("h1.title").innerHTML = "Nå er vi så jævlig nærme!";
+        document.querySelector(".text.before").innerHTML = "Det er bare";
+        document.querySelector(".text.after").innerHTML = "Til helgen nå";
+
+        if (hours == 0) {
             document.querySelector(".hour").style.display = "none";
-            document.querySelector(".milliseconds").style.display = "flex";
+            document.querySelector(".time-container.millisecond").style.display = "flex";
+
             document.querySelector("h1.title").innerHTML = "Siste nedtelling!";
             document.querySelector(".text.before").innerHTML = "Det er bare";
             document.querySelector(".text.after").innerHTML = "Til helgen nå";
 
-            document.querySelector(".milliseconds .time").innerHTML = 100 - Math.floor(date.getMilliseconds() / 10);
+            if (minutes < 0) {
+                document.querySelectorAll(".time-container").forEach(e => e.style.display = "none" );
+
+                document.querySelector("h1.title").innerHTML = "DET ER HELG!";
+                document.querySelector(".text.before").innerHTML = "Gå ut av kontoret";
+                document.querySelector(".text.after").innerHTML = "Og skrik hallelujah";
+
+                if (celebratecount < 1) {
+                    celebrate();
+                    celebratecount++;
+                }
+            }
         }
-        else {
-            document.querySelector(".hour .time").innerHTML = weekend[1] - date.getHours();
+        else if (hours < 0) {
+            document.querySelectorAll(".time-container").forEach(e => e.style.display = "none" );
+
+            document.querySelector("h1.title").innerHTML = "DET ER HELG!";
+            document.querySelector(".text.before").innerHTML = "Og det er fortsatt";
+            document.querySelector(".text.after").innerHTML = "FREDAG!";
+
+            if (celebratecount < 1) {
+                celebrate();
+                celebratecount++;
+            }
         }
     }
+    else if (days < 0) {
+        document.querySelectorAll(".time-container").forEach(e => e.style.display = "none" );
 
-    document.querySelector(".minute .time").innerHTML = weekend[2] - date.getMinutes() < 0 
-        ? weekend[2] - date.getMinutes() + 60 : weekend[2] - date.getMinutes();
-        
-    document.querySelector(".second .time").innerHTML =  weekend[3] - date.getSeconds() < 0 
-        ? weekend[3] - date.getSeconds() + 60 : weekend[3] - date.getSeconds();
+        document.querySelector("h1.title").innerHTML = "DET ER FORTSATT HELG!";
+        document.querySelector(".text.before").innerHTML = "Legg deg seint";
+        document.querySelector(".text.after").innerHTML = "Våkne seinere!";
+
+        if (celebratecount < 1) {
+            celebrate();
+            celebratecount++;
+        }
+    }
+    else if (days == 1) {
+        document.querySelector("h1.title").innerHTML = "Endelig snart helg nå!";
+        document.querySelector(".text.before").innerHTML = "Det er enda";
+    }
 }
 
-setInterval(newTime, 1);
+if (countdownType == "weekend")
+    setInterval(() => updateTimeWeekend(countdownDate), 10)
+
+else
+    setInterval(() => updateTimeDate(countdownDate), 10)
+    
